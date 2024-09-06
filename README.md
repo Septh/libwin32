5f00ca0fbfabc298ecde0904c6383ae096a8b4e0
Stephan Schreiber <septh@users.noreply.github.com>
2024-09-06 10:12:41 +0200

    chore: Various file changes

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-## TODO
+# libwin32 (work in progress)
+> Node bindings to native Win32 DLLs through [Koffi](https://koffi.dev).
+
+### In a nutshell:
+* Very simple and intuitive API (see [demo](./source//demos/window.ts)).
+* Bundler friendly, designed with tree-shakeability in mind.
+* Opinionated:
+    * Only targets **win32-x64** platforms (*ARM-64 may be added later, no warranty though*).
+    * Only exposes **Unicode** functions and data structures (those whose name ends in `W` in the Win32 API).
+* Very easy to extend with additional functions, data structures and constants. I will add some myself time permitting; any help would be *mucho* appreciated.
+* Not yet published to the npm registry. Will do when the library is large enough. Meanwhile, either clone this repo or run `nmp install septh/libwin32`.
+
+
+### Directories
+* `./source/demos`:
+    * Some usage examples. (*only one at the time, more to come later*)
+* `./source/rollup`:
+    * A [Rollup](https://rollup.org) plugin to ease the process of building this library with your code. This targets the *consumers* of the library, not the library itself. (*more info to come*).
+* `./source/stubs`:
+    * Koffi loading & bundling stub.
+* `./source/win32`:
+    * The main bindings source files.
+
+
+### How to...
+* Build:
+    * Use TypeScript (version 5.2+ required).
+    * Run `npm run build` at the repo's root.
+    * The output goes to `/dist`.
+* See the demo:
+    * Build, then run `node dist/demos/window.js`.
---
+++ source/demos/window.ts
@@ -0,0 +1,72 @@
+import {
+    WNDCLASSEX, IDC, IDI, CS, WS, CW, WM, SW, MB,
+    type HINSTANCE, type WPARAM, type LPARAM, type HWND, type MSG
+} from 'libwin32'
+import * as kernel32 from 'libwin32/kernel32'
+import * as user32 from 'libwin32/user32'
+
+const windowClass = "NodeApp"
+const windowName  = "Window Demo !"
+const appTitle    = "A NodeJS app using the Win32 API"
+
+function wndProc(hWnd: HWND, uMmsg: WM, wParam: WPARAM, lParam: LPARAM) {
+
+    let ret: number
+    switch (uMmsg) {
+        case WM.DESTROY:
+            user32.PostQuitMessage(0)
+            ret = 0
+            break
+
+        default:
+            ret = user32.DefWindowProc(hWnd, uMmsg, wParam, lParam)
+            break
+    }
+    // console.log(hWnd, { uMmsg, wParam, lParam }, ret)
+    return ret
+}
+
+function WinMain(hInstance: HINSTANCE, nCmdShow: SW): number {
+
+    using wcex = new WNDCLASSEX(wndProc)
+    wcex.lpszClassName = windowClass
+    wcex.style         = CS.HREDRAW | CS.VREDRAW
+    wcex.hInstance     = hInstance
+    wcex.hCursor       = user32.LoadCursor(null, IDC.ARROW)
+    wcex.hIcon         = user32.LoadIcon(hInstance, IDI.APPLICATION)
+    wcex.hIconSm       = user32.LoadIcon(hInstance, IDI.APPLICATION)
+    wcex.hbrBackground = 13 as any
+
+    const atom = user32.RegisterClassEx(wcex)
+    if (!atom) {
+        user32.MessageBox(null, "Call to RegisterClassEx failed!", appTitle, MB.OK | MB.ICONERROR)
+        return 1
+    }
+
+    const hWnd = user32.CreateWindow(
+        windowClass, windowName,
+        WS.CAPTION | WS.SYSMENU,
+        CW.USEDEFAULT, CW.USEDEFAULT, 600, 400,
+        null, null, hInstance, null
+    )
+    if (!hWnd) {
+        const err = kernel32.GetLastError()
+        user32.MessageBox(null, "Call to CreateWindow failed!\n" + err.toString(16), appTitle, MB.OK | MB.ICONERROR)
+        return -1
+    }
+
+    user32.ShowWindow(hWnd, nCmdShow)
+    user32.UpdateWindow(hWnd)
+
+    // Main message loop
+    const msg = {} as MSG
+    while (user32.GetMessage(msg, null, 0, 0)) {
+        user32.TranslateMessage(msg)
+        user32.DispatchMessage(msg)
+    }
+
+    return 0
+}
+
+debugger
+process.exitCode = WinMain(kernel32.GetModuleHandleW(null), SW.NORMAL)
