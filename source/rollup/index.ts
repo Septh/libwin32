9c6e1a9885433e97bf736d53655b89bb6dae2206
Stephan Schreiber <septh@users.noreply.github.com>
2025-05-21 00:49:42 +0200

    build(demos): remove the rollup plugin for koffi

--- source/rollup/index.ts
+++ source/rollup/index.ts
@@ -1,10 +1,8 @@
 import type { Plugin } from 'rollup'
 import { enums } from './plugin-enums.js'
-// import { koffi } from './plugin-koffi.js'
 
 export const libwin32: () => Plugin[] = () => [
     enums(),
-    // koffi(),
 ]
 
 export default libwin32
