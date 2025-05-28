8b1093481bb0d0446740ba6d173744031efca381
Stephan Schreiber <septh@users.noreply.github.com>
2025-05-29 01:14:17 +0200

    refactor(libs): add sanity test on internal library reference

--- source/win32/private.ts
+++ source/win32/private.ts
@@ -3,21 +3,23 @@ import koffi from 'koffi-cream'
 
 export { koffi }
 
-export const textDecoder = /*#__PURE__*/new TextDecoder('utf-16')
+export const textDecoder  = /*#__PURE__*/new TextDecoder('utf-16')
 
 export class Win32Dll implements Disposable {
     get x64() { return true }
     get Unicode() { return true }
-    #lib: koffi.IKoffiLib
+    #dll: string
+    #lib?: koffi.IKoffiLib
 
-    constructor(dllName: string) {
-        assert(this.#lib = koffi.load(dllName), `Could not load ${JSON.stringify(dllName)}.`)
+    constructor(dll: string) {
+        this.#dll = dll
+        assert(this.#lib = koffi.load(dll), `Could not load ${JSON.stringify(dll)}.`)
     }
 
     // For use with the `using` syntax (requires TypeScript 5.2+)
     [Symbol.dispose]() {
         this.#lib?.unload()
-        this.#lib = undefined!
+        this.#lib = undefined
     }
 
     unload() {
@@ -25,6 +27,7 @@ export class Win32Dll implements Disposable {
     }
 
     func(name: string, result: koffi.IKoffiCType, parameters: koffi.IKoffiCType[]) {
+        assert(this.#lib, `${JSON.stringify(this.#dll)} has been unloaded.`)
         return /*#__PURE__*/this.#lib.func(name, result, parameters)
     }
 }
