bb03d989aa67685d1fcb00d924d5f6c8e36751a3
Stephan Schreiber <septh@users.noreply.github.com>
2025-08-11 14:28:07 +0200

    style(advapi32): clean decodeSid()

--- source/win32/advapi32.ts
+++ source/win32/advapi32.ts
@@ -107,10 +107,9 @@ function decodeSid(sidPtr: unknown): SID {
 
     // Decode the exact number of sub-authorities present in the SID. Yet, we still must set the array length
     // to SID_MAX_SUB_AUTHORITIES otherwise calls to other functions that expect a SID parameter would fail.
-    const SubAuthority: number[] = [
-        ...koffi.decode(sidPtr, koffi.offsetof(cSID, 'SubAuthority'), cDWORD, SubAuthorityCount),
-        ...Array(Internals.SID_MAX_SUB_AUTHORITIES - SubAuthorityCount).fill(0)
-    ]
+    const SubAuthority: number[] = koffi.decode(sidPtr, koffi.offsetof(cSID, 'SubAuthority'), cDWORD, SubAuthorityCount).slice()    // Need to slice() because .length is read-only :(
+    SubAuthority.length = Internals.SID_MAX_SUB_AUTHORITIES
+    SubAuthority.fill(0, SubAuthorityCount)
 
     // Return the decoded SID.
     return {
