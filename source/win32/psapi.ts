538394a3378b37fadadd975297f42769316b9cd2
Stephan Schreiber <septh@users.noreply.github.com>
2025-08-13 05:51:13 +0200

    perf(psapi): simplify functions

--- source/win32/psapi.ts
+++ source/win32/psapi.ts
@@ -1,5 +1,5 @@
 import koffi from 'koffi-cream'
-import { Win32Dll, type OUT } from './private.js'
+import { Win32Dll } from './private.js'
 import {
     cBOOL, cDWORD,
     cHANDLE, type HANDLE} from './ctypes.js'
@@ -19,7 +19,7 @@ export function GetProcessMemoryInfo(hProcess: HANDLE): PROCESS_MEMORY_COUNTERS_
     GetProcessMemoryInfo.native ??= psapi.func('GetProcessMemoryInfo', cBOOL, [ cHANDLE, koffi.out(koffi.pointer(cPROCESS_MEMORY_COUNTERS_EX2)), cDWORD ])
 
     const memCounters = new PROCESS_MEMORY_COUNTERS_EX2()
-    if (GetProcessMemoryInfo.native(hProcess, memCounters, koffi.sizeof(cPROCESS_MEMORY_COUNTERS_EX2)) !== 0)
-        return memCounters
-    return null
+    return GetProcessMemoryInfo.native(hProcess, memCounters, koffi.sizeof(cPROCESS_MEMORY_COUNTERS_EX2)) !== 0
+        ? memCounters
+        : null
 }
