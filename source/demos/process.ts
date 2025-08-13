e8a30e057be07c3cffbb76f2b3f59063900bea2d
Stephan Schreiber <septh@users.noreply.github.com>
2025-08-13 11:20:10 +0200

    feat(demos): extend process example

--- source/demos/process.ts
+++ source/demos/process.ts
@@ -1,6 +1,7 @@
 import {
     GetForegroundWindow, GetWindowThreadProcessId, GetWindowText,
-    OpenProcess, QueryFullProcessImageName, CloseHandle
+    OpenProcess, QueryFullProcessImageName, CloseHandle,
+    GetProcessMemoryInfo
 } from 'libwin32'
 import { PSAR_ } from 'libwin32/consts'
 
@@ -11,13 +12,17 @@ const { processId, threadId } = GetWindowThreadProcessId(hWnd)
 const hProc = OpenProcess(PSAR_.PROCESS_QUERY_INFORMATION | PSAR_.PROCESS_VM_READ, false, processId)
 
 let executable
+let meminfo
 if (hProc) {
     executable = QueryFullProcessImageName(hProc, 0)
+    meminfo = GetProcessMemoryInfo(hProc)
     CloseHandle(hProc)
 }
 
 console.log(`Foreground window is "${windowText}"
     processId: ${processId}
     threadId: ${threadId}
-    executable: ${executable}`
+    executable: ${executable}
+`
 )
+console.log("GetProcessMemoryInfo():\n", meminfo)
